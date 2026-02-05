2123fee29af944d06da67e21b26813b650c3d191
Crystal0404 <120556221+Crystal0404@users.noreply.github.com>
2026-02-05 15:28:26 +0800

    support beta-type

--- .github/workflows/script/matrix.py
+++ .github/workflows/script/matrix.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import re
 import json
 import tomllib
 
@@ -9,19 +10,29 @@ def main():
     mc_vers = []
     dir_map = {}
     ver_map = {}
+    type_map = {}
     for mc_dir in mc_dirs:
         path = "{}/pack.toml".format(mc_dir)
         with open(path, "rb") as f:
             data = tomllib.load(f)
         mc_ver = data["versions"]["minecraft"]
+        pack_ver = data["version"]
         mc_vers.append(mc_ver)
         dir_map[mc_ver] = mc_dir
-        ver_map[mc_ver] = data["version"]
+        ver_map[mc_ver] = pack_ver
+        if re.match(".*alpha\\.\\d+", pack_ver):
+            raise ValueError
+        if re.match(".*(beta|rc)\\.\\d+", pack_ver):
+            type_map[mc_ver] = "beta"
+        else:
+            type_map[mc_ver] = "release"
+
 
     json_data = {
         "mc_vers": mc_vers,
         "dir_map": dir_map,
-        "ver_map": ver_map
+        "ver_map": ver_map,
+        "type_map": type_map
     }
 
     with open("data.json", "w", encoding="utf-8") as f:
