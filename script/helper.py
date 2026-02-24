7255337a7b995206844c12d415b449105dd8e7f9
Crystal0404 <120556221+Crystal0404@users.noreply.github.com>
2026-02-24 19:17:35 +0800

    add stop command

--- script/helper.py
+++ script/helper.py
@@ -7,7 +7,11 @@ import shlex
 def run():
     from script.call import call, From
     completer = NestedCompleter.from_nested_dict({
+        "stop": None,
         "import": {"-platform": {"mr", "cf", "all"}}
     })
-    text = prompt('', completer=completer)
-    call(arg=shlex.split(text), by=From.HELPER)
+    while True:
+        text = prompt('', completer=completer)
+        if text == "stop":
+            break
+        call(arg=shlex.split(text), by=From.HELPER)
