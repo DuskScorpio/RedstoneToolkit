8cdeedc642929e96820db027a7324d5de4054e18
Crystal0404 <120556221+Crystal0404@users.noreply.github.com>
2026-04-18 22:42:51 +0800

    add help

--- script/utils/call.py
+++ script/utils/call.py
@@ -14,65 +14,71 @@ def __register_arg(arg: list[str] | None = None) -> Namespace:
     subparsers = parser.add_subparsers(dest="command")
 
     # helper
-    subparsers.add_parser("helper")
+    subparsers.add_parser("helper", description="Start helper")
 
     # import
-    parser_import = subparsers.add_parser("import")
+    parser_import = subparsers.add_parser("import", description="Import mods from the '.index' folder")
     parser_import.add_argument(
         "--platform",
         choices=[PlatForm.MODRINTH, PlatForm.CURSEFORGE, PlatForm.ALL],
-        default=PlatForm.MODRINTH
+        default=PlatForm.MODRINTH,
+        help="default 'modrinth'"
     )
 
     # install
-    parser_install = subparsers.add_parser("install")
+    parser_install = subparsers.add_parser("install", description="parse and download files from 'file_list'")
     parser_install.add_argument(
         "--platform",
         choices=[PlatForm.MODRINTH, PlatForm.CURSEFORGE, PlatForm.ALL],
-        default=PlatForm.ALL
+        default=PlatForm.ALL,
+        help="default 'all'"
     )
-    parser_install.add_argument("--versions")
+    parser_install.add_argument("--versions", help="Your version folder name, if empty select all")
 
     # create
-    parser_create = subparsers.add_parser("create").add_mutually_exclusive_group()
-    parser_create.add_argument("--versions")
-    parser_create.add_argument("--snapshot", action="store_true")
+    parser_create = subparsers.add_parser("create", description="Create a Minecraft version").add_mutually_exclusive_group()
+    parser_create.add_argument("--versions", help="Minecraft version, if there are multiple uses ',' split")
+    parser_create.add_argument("--snapshot", action="store_true", help="Use the latest snapshot")
 
     # remove
-    parser_remove = subparsers.add_parser("remove")
+    parser_remove = subparsers.add_parser("remove", description="Removed the Minecraft version")
     parser_remove.add_argument(
         "--versions",
-        required=True
+        required=True,
+        help="The name of the Minecraft folder that needs to be removed, if there are multiple uses ',' split"
     )
 
     # update
-    parser_update = subparsers.add_parser("update")
-    parser_update.add_argument("--version")
+    parser_update = subparsers.add_parser("update", description="Update mod")
+    parser_update.add_argument("--version", help="The name of the Minecraft folder")
 
     # export
-    parser_export = subparsers.add_parser("export")
-    parser_export.add_argument("--version")
+    parser_export = subparsers.add_parser("export", description="Export the integration package")
+    parser_export.add_argument("--version", help="The name of the Minecraft folder")
     parser_export.add_argument(
         "--platform",
         choices=[PlatForm.MODRINTH, PlatForm.CURSEFORGE, PlatForm.ALL],
-        default=PlatForm.ALL
+        default=PlatForm.ALL,
+        help="default 'all'"
     )
 
     # refresh
-    subparsers.add_parser("refresh")
+    subparsers.add_parser("refresh", description="Refresh everything")
 
     # update loader
-    subparsers.add_parser('loader')
+    subparsers.add_parser("loader", description="Update fabric_loader to the latest version")
 
     # update version
-    parser_update_version = subparsers.add_parser("update_version")
+    parser_update_version = subparsers.add_parser("update_version", description="Update the version of the 'pack.toml'")
     parser_update_version.add_argument(
         "--match",
-        default="*"
+        default="*",
+        help="NPM-compliant version matching, default '*'"
     )
     parser_update_version.add_argument(
         "--version",
-        required=True
+        required=True,
+        help="e.g: X.Y.Z"
     )
 
     args = parser.parse_args(arg)
